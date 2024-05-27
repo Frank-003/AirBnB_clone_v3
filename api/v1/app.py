b4b2462c13ef6cd518326b0172766478f660cd99
Frank-003 <oyiengofrankline49@gmail.com>
2024-05-27 14:43:36 +0000

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,38 @@
-import os
-from api.v1.app import app
-from api.v1.views import *
-import unittest
-import tempfile
-import flask
+#!/usr/bin/python3
+"""app"""
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 
-class AppTestCase(unittest.TestCase):
-    '''test app.py'''
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
-    def test_create_app(self):
-        '''check app instance with blueprint is created'''
-        with app.test_client() as c:
-            self.assertIsInstance(c, flask.testing.FlaskClient)
 
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear(self):
+    ''' closes storage engine '''
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    unittest.main()
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
